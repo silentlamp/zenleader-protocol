ffe852586981f09892caa46ad45304da6fd606a8
silentlamp <tienhuynhn2004@gmail.com>
2026-05-06 02:10:45 +0700

    chore: changed to double

--- plugnmeet/plugnmeet_analytics.pb.validate.go
+++ plugnmeet/plugnmeet_analytics.pb.validate.go
@@ -61,6 +61,10 @@ func (m *AnalyticsDataMsg) validate(all bool) error {
 
 	// no validation rules for EventName
 
+	// no validation rules for RoomId
+
+	// no validation rules for Time
+
 	if m.EventValueString != nil {
 		// no validation rules for EventValueString
 	}
@@ -73,10 +77,6 @@ func (m *AnalyticsDataMsg) validate(all bool) error {
 		// no validation rules for HsetValue
 	}
 
-	if m.RoomId != nil {
-		// no validation rules for RoomId
-	}
-
 	if m.RoomSid != nil {
 		// no validation rules for RoomSid
 	}
@@ -87,10 +87,6 @@ func (m *AnalyticsDataMsg) validate(all bool) error {
 
 	if m.UserName != nil {
 		// no validation rules for UserName
-	}
-
-	if m.Time != nil {
-		// no validation rules for Time
 	}
 
 	if m.ExtraData != nil {
@@ -454,6 +450,8 @@ func (m *AnalyticsRoomInfo) validate(all bool) error {
 	// no validation rules for RoomEnded
 
 	// no validation rules for RoomDuration
+
+	// no validation rules for EnabledE2Ee
 
 	for idx, item := range m.GetEvents() {
 		_, _ = idx, item
